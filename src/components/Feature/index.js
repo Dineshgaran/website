524f39b17cc43ce5831588b3687220a24c61faee
unknown <Dinesh@DINESH-IT.alf.lk>
2021-03-16 13:56:22 +0530

    Added files

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './FeatureElements';
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+      <h1>Tasty Burgers ! yummyy!!!</h1>
+      <p>We are serving tasty and deliciouse Burgers for our Customers.</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+};
+
+export default Feature;
---
+++ src/components/Products/data.js
@@ -0,0 +1,66 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Burger',
+    name: 'Chicken Burger',
+    desc:
+      ' Crispy chicken with cheese slice & sauce etc!....',
+    price: 'Rs.450.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Burger',
+    name: 'Ham Burger',
+    desc:
+      ' Crispy chicken with cheese slice & sauce etc!....',
+    price: 'Rs.350.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Burger',
+    name: 'Veggie Burger',
+    desc:
+      ' Fresh  veggie with cheese slice & sauce etc!....',
+    price: 'Rs.250.99',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donuts',
+    name: 'Dough Donut',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: 'Rs.100.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc:
+      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: 'Rs.100.99',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet1,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: 'Rs.100.99',
+    button: 'Add to Cart'
+  }
+];
